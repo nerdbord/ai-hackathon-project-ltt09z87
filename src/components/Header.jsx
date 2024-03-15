56cb39bb7a81f902d858e50afa92fe8aff2ba453
sniperdev <igorspychalaa@gmail.com>
2024-03-15 22:31:23 +0100

    add header

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+	return (
+		<div className="flex flex-col items-center gap-y-4">
+			<img src="/logo2.webp" alt="" width="200px"/>
+			<h2 className="text-2xl">Decyzyjnik</h2>
+		</div>
+	);
+};
+
+export default Header;
