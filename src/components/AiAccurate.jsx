295b53b409273a51aad6554d758acb2e2849e898
sniperdev <igorspychalaa@gmail.com>
2024-03-16 00:54:47 +0100

    fix AiAccurate component add api response

--- src/components/AiAccurate.jsx
+++ src/components/AiAccurate.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
+
 const AiAccurate = () => {
+  const [answer, setAnswer] = useState("Ziemniaki");
   return (
     <div className="flex flex-col items-center justify-center gap-y-4 h-full">
       <h1>Czy Ai dobrze wybrało kategorie produktu?</h1>
+      <p className="font-bold">{answer}</p>
       <div className="flex gap-x-2">
         <button className="btn btn-success">Tak</button>
         <button className="btn btn-error">Nie</button>
