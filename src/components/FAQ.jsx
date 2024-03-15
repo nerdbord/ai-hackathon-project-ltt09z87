d808109ba7bc4453fe8676a271784e38d909bca2
Kamil <117399144+Cr3ativeCod3r@users.noreply.github.com>
2024-03-15 23:14:28 +0100

    Update FAQ.jsx

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,44 +1,50 @@
 // FAQ
-import React from 'react';
-
-const FAQ = ({ isOpen, handleClose }) => {
-  const faqs = [
-    {
-      header: "Często zadawane pytanie 1",
-      description: "Odpowiedź na często zadawane pytanie 1 znajduje się tutaj.",
-    },
-    {
-      header: "Często zadawane pytanie 2",
-      description: "Odpowiedź na często zadawane pytanie 2 znajduje się tutaj.",
-    },
-    {
-        header: "Często zadawane pytanie 3",
-        description: "Odpowiedź na często zadawane pytanie 2 znajduje się tutaj.",
-      },
-    
-  ];
-
+import React from "react";
+const faqs = [
+  {
+    header: "Często zadawane pytanie 1",
+    description: "Odpowiedź na często zadawane pytanie 1 znajduje się tutaj.",
+  },
+  {
+    header: "Często zadawane pytanie 2",
+    description: "Odpowiedź na często zadawane pytanie 2 znajduje się tutaj.",
+  },
+  {
+    header: "Często zadawane pytanie 3",
+    description: "Odpowiedź na często zadawane pytanie 2 znajduje się tutaj.",
+  },
+];
+export const FAQ = () => {
   return (
     <>
-      {isOpen && (
-        <div className="fixed inset-0 flex justify-center items-center z-50 bg-black bg-opacity-50" onClick={handleClose}>
-          <div className="bg-gray-800 rounded-lg shadow-lg max-w-md w-full overflow-y-auto max-h-full p-4 text-center" onClick={(e) => e.stopPropagation()}>
-            <button onClick={handleClose}>x</button>
-            <h1 className="text-slate-400 text-2xl font-bold mb-4">Najczęściej zadawane pytania</h1>
-            {faqs.map((faq, index) => (
-              <SingleFAQ key={index} header={faq.header} description={faq.description} />
-            ))}
-          </div>
+      <label htmlFor="my_modal_7" className="btn">
+        open modal
+      </label>
+      <input type="checkbox" id="my_modal_7" className="modal-toggle" />
+      <div className="modal" role="dialog">
+        <div className="modal-box">
+          <h1 className="text-slate-400 text-2xl font-bold mb-4">
+            Najczęściej zadawane pytania
+          </h1>
+          {faqs.map((faq, index) => (
+            <SingleFAQ
+              key={index}
+              header={faq.header}
+              description={faq.description}
+            />
+          ))}
         </div>
-      )}
+        <label className="modal-backdrop" htmlFor="my_modal_7">
+          Close
+        </label>
+      </div>
     </>
   );
 };
-
 const SingleFAQ = ({ header, description }) => {
   return (
     <>
-      <hr className="my-4"/>
+      <hr className="my-4" />
       <div>
         <h2 className="text-xl font-bold mb-2">{header}</h2>
         <p className="text-sm">{description}</p>
@@ -46,5 +52,3 @@ const SingleFAQ = ({ header, description }) => {
     </>
   );
 };
-
-export default FAQ;
