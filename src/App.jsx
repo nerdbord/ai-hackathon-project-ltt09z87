50a2dacacfe736bf5049c78e839f5acdcf2ecbe4
Kamil <117399144+Cr3ativeCod3r@users.noreply.github.com>
2024-03-15 23:13:57 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,11 @@ import FAQ from './components/FAQ';
 import {useState} from "react";
 
 function App() {
-//FAQ	
-  const [isOpen, setIsOpen] = useState(false);
-  const handleToggleFAQ = () => {
-    setIsOpen(!isOpen);
-  };
-
   return (
 		<>
       <MainLayout>
-        <SelectInput/>
-	<FAQ isOpen={isOpen} handleClose={handleToggleFAQ} /> {!isOpen && (<button onClick={handleToggleFAQ}>Otwórz FAQ</button>)}
+      <SelectInput/>
+	    <FAQ/> 
       </MainLayout>
 		</>
 	);
