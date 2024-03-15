da7555808ab53c6a794c9ce72fa008e630f788b5
Kamil <117399144+Cr3ativeCod3r@users.noreply.github.com>
2024-03-15 22:05:37 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import './App.css'
 import { MainLayout } from './components/MainLayout';
 import { SelectInput } from './components/SelectInput';
+import FAQ from './components/FAQ';
 
 function App() {
+//FAQ	
+  const [isOpen, setIsOpen] = useState(false);
+  const handleToggleFAQ = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
 		<>
       <MainLayout>
         <SelectInput/>
+	<FAQ isOpen={isOpen} handleClose={handleToggleFAQ} /> {!isOpen && (<button onClick={handleToggleFAQ}>Otwórz FAQ</button>)}
       </MainLayout>
 		</>
 	);
