b7ddf0979cdf3d00c20f1fc769ce1ed09ffc6ab8
Cr3ativeCod3r <banaszekk123@gmial.com>
2024-03-15 23:45:08 +0100

    loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
 import './App.css'
+import { MainLayout } from './components/MainLayout';
+import { SelectInput } from './components/SelectInput';
+import {FAQ} from './components/FAQ';
+import {Loader} from './components/Loader';
+import {useState,useEffect} from "react";
+
+
 
 function App() {
+  
+const [isLoading, setIsLoading] = useState(true);
+useEffect(() => {
+  setTimeout(() => {
+    setIsLoading(false);
+  }, 1000);
+}, []);
+
   return (
 		<>
+     {isLoading ? (
+        <Loader />
+      ) : (
       <MainLayout>
       <SelectInput/>
 	    <FAQ/> 
       </MainLayout>
+       )}
 		</>
 	);
 }
---
+++ src/components/Loader.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Loader = () => {
+  return (
+    <div className="loader-container">
+     <span className="loading loading-infinity loading-lg"></span>
+    </div>
+  );
+}
+
+
