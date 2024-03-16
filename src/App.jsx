33e3fedb90533ed7c1ae00a6878d091053a485a7
Wiecek-K <113645679+Wiecek-K@users.noreply.github.com>
2024-03-16 13:03:21 +0100

    conflict resolved

--- src/App.jsx
+++ src/App.jsx
@@ -24,30 +24,7 @@ function App() {
   assignCategoryViaChat(
     'Ręcznik papierowy z celulozy Słonik Jumbo Maxi 300 listków dwuwarstwowy biały',
   )
-  const products = [
-    'SMACZNY ZIEMNIAK ŻÓŁTY JADALNY KARTOFLE PYRY SORAYA ZIEMNIAKI 15KG POLSKIE',
-    'Ręcznik papierowy z celulozy Słonik Jumbo Maxi 300 listków dwuwarstwowy biały',
-    // 'Smartwatch Samsung Galaxy Watch',
-    'Konsola do gier PlayStation 5',
-    'Zestaw garnków Tefal',
-    // 'Perfumy Dior Sauvage',
-    'Komplet pościeli z bawełny egipskiej',
-    'Rower górski Scott Aspect',
-    // 'Zestaw kamer bezprzewodowych Arlo',
-    // 'Blender kuchenny Philips',
-    'Odkurzacz bezworkowy Dyson',
-    // 'Smartfon iPhone 12 Pro',
-    // 'Kamera GoPro Hero 9 Black',
-    // 'Słuchawki douszne JBL',
-    // 'Laptop Lenovo ThinkPad',
-    'Akumulatorowy wiertarko-wkrętarka Bosch',
-    // 'Ekspres do kawy Nespresso',
-    // 'Monitor komputerowy LG UltraGear',
-    // 'Tablet Samsung Galaxy Tab S7',
-    // 'Nawigacja samochodowa Garmin',
-    // 'Robot kuchenny KitchenAid',
-    'Zestaw piłkarski Adidas Tango',
-  ]
+
   // products.forEach(product=>generateChatCompletion(product))
 
   return (
