8427c706e9ef4da4d2d790e9f91cda046aa2aca7
Wiecek-K <113645679+Wiecek-K@users.noreply.github.com>
2024-03-16 04:23:50 +0100

    added function assigncategoryviachat

--- API/index.js
+++ API/index.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
-
+import { categories } from './categories'
 export const OPEN_AI_KEY =
   '4bebe73e0e37a06315d78e10c5c8c2f8abfd5e5b7f4112b12fdc7bef19cdc5af47ab1bc43b5300a231b9a1cd05f07ef6b7bd5b59f7fb2df6a098b0ef74cd784c'
 
 const openaiAPI = axios.create({
-  baseURL: 'https://training.nerdbord.io/api/v1/openai',
+  baseURL: 'https://training.nerdbord.io/api/v1/openai/chat/completions',
   headers: {
     'Content-Type': 'application/json',
     Authorization: OPEN_AI_KEY,
@@ -12,15 +12,14 @@ const openaiAPI = axios.create({
   timeout: 60000,
 })
 
-async function generateChatCompletion(content) {
+async function assignCategoryViaChat(content) {
   try {
-    const response = await openaiAPI.post('/chat/completions', {
+    const product = await openaiAPI.post('', {
       model: 'gpt-3.5-turbo',
       messages: [
         {
           role: 'system',
-          content:
-            'You are a helpful assistant. Answer in polish. Return only stringify JSON. Categorize the item provided by the user into one of the following categories: electronics, clothes, hobbies, food',
+          content: `You are a helpful assistant categorizing products from web stores. Return only stringify JSON. Response is a object according the structure: {nameOnWebStores, whatIsIt}. Find inside the auction name what type of product the auction concerns.`,
         },
         {
           role: 'user',
@@ -28,9 +27,23 @@ async function generateChatCompletion(content) {
         },
       ],
     })
-    // return response.data as ChatCompletionResponse
-    console.log('RESPONSE', response)
-    console.log('DATA', response.data)
+    console.log(JSON.parse(product.data.choices[0].message.content).whatIsIt);
+    
+    const response = await openaiAPI.post('', {
+      model: 'gpt-3.5-turbo',
+      messages: [
+        {
+          role: 'system',
+          content: `You are a helpful assistant categorizing products in stores. Return only stringify JSON. Response is a object according the structure: {itemName, category} Categorize the item provided by the user into one of the following categories separated by a comma. Categories: ### ${categories}. ### Put every hand tool into category: hobby`,
+        },
+        {
+          role: 'user',
+          content: JSON.parse(product.data.choices[0].message.content).whatIsIt,
+        },
+      ],
+    })
+
+    console.log('DATA', response.data.choices[0].message.content)
 
     return response.data
   } catch (error) {
@@ -38,4 +51,4 @@ async function generateChatCompletion(content) {
   }
 }
 
-export { generateChatCompletion }
+export { assignCategoryViaChat }
--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,37 @@ import './App.css'
 import { MainLayout } from './components/MainLayout'
 import { SelectInput } from './components/SelectInput'
 import { FAQ } from './components/FAQ'
-import { generateChatCompletion } from '../API'
+import { assignCategoryViaChat } from '../API'
 
 function App() {
-  generateChatCompletion(
-    'SMACZNY ZIEMNIAK ŻÓŁTY JADALNY KARTOFLE PYRY SORAYA ZIEMNIAKI 15KG POLSKIE',
+  assignCategoryViaChat(
+    'Ręcznik papierowy z celulozy Słonik Jumbo Maxi 300 listków dwuwarstwowy biały',
   )
+  const products = [
+    'SMACZNY ZIEMNIAK ŻÓŁTY JADALNY KARTOFLE PYRY SORAYA ZIEMNIAKI 15KG POLSKIE',
+    'Ręcznik papierowy z celulozy Słonik Jumbo Maxi 300 listków dwuwarstwowy biały',
+    // 'Smartwatch Samsung Galaxy Watch',
+    'Konsola do gier PlayStation 5',
+    'Zestaw garnków Tefal',
+    // 'Perfumy Dior Sauvage',
+    'Komplet pościeli z bawełny egipskiej',
+    'Rower górski Scott Aspect',
+    // 'Zestaw kamer bezprzewodowych Arlo',
+    // 'Blender kuchenny Philips',
+    'Odkurzacz bezworkowy Dyson',
+    // 'Smartfon iPhone 12 Pro',
+    // 'Kamera GoPro Hero 9 Black',
+    // 'Słuchawki douszne JBL',
+    // 'Laptop Lenovo ThinkPad',
+    'Akumulatorowy wiertarko-wkrętarka Bosch',
+    // 'Ekspres do kawy Nespresso',
+    // 'Monitor komputerowy LG UltraGear',
+    // 'Tablet Samsung Galaxy Tab S7',
+    // 'Nawigacja samochodowa Garmin',
+    // 'Robot kuchenny KitchenAid',
+    'Zestaw piłkarski Adidas Tango',
+  ]
+  // products.forEach(product=>generateChatCompletion(product))
 
   return (
     <>
