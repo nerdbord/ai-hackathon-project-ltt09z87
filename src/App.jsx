ed52f1888582c7db64327859d5a3419f3ea3bd18
sniperdev <igorspychalaa@gmail.com>
2024-03-16 01:12:10 +0100

    create form with steps

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { MainLayout } from "./components/MainLayout";
 import AiAccurate from "./components/AiAccurate.jsx";
+import FormComponent from "./components/FormComponent/FormComponent.jsx";
 
 function App() {
   return (
     <>
       <MainLayout>
-        <AiAccurate />
+        {/*<AiAccurate />*/}
+        <FormComponent />
       </MainLayout>
     </>
   );
